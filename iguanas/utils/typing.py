70a6d72e8497972e2536b34d4aeb7a8d62136494
James Laidler <jlaidler@paypal.com>
2022-01-05 09:25:54 +0000

    Tried TYPE_CHECKING

--- iguanas/utils/typing.py
+++ iguanas/utils/typing.py
@@ -1,5 +1,11 @@
 """Custom typing objects. Used so packages don't need to be imported."""
 from typing import TypeVar
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#     import numpy
+#     import pandas
+#     import databricks
+#     import pyspark
 
 NumpyArrayType = TypeVar('numpy.ndarray')
 PandasDataFrameType = TypeVar('pandas.core.frame.DataFrame')
@@ -7,3 +13,10 @@ PandasSeriesType = TypeVar('pandas.core.series.Series')
 KoalasDataFrameType = TypeVar('databricks.koalas.frame.DataFrame')
 KoalasSeriesType = TypeVar('databricks.koalas.series.Series')
 PySparkDataFrameType = TypeVar('pyspark.sql.dataframe.DataFrame')
+
+# NumpyArrayType = 'numpy.ndarray'
+# PandasDataFrameType = 'pandas.core.frame.DataFrame'
+# PandasSeriesType = 'pandas.core.series.Series'
+# KoalasDataFrameType = 'databricks.koalas.frame.DataFrame'
+# KoalasSeriesType = 'databricks.koalas.series.Series'
+# PySparkDataFrameType = 'pyspark.sql.dataframe.DataFrame'
